cc5e2b7cb12cd378d518188cc38b9f92cfa97e94
Matthew Thornton <53791828+Matthew-Thornton@users.noreply.github.com>
2021-03-29 09:48:04 -0500

    fixed db name

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,8 +1,8 @@
 <?php
 	$servername = "localhost:3306";
-	$dbUsername = "YOURUSERNAMEHERE";
+	$dbUsername = "mahs_mthornton";
 	$dbPassword = "mustangs";
-	$dbName = "YOURUSERNAMEHERE";
+	$dbName = "mahs_mthornton";
 	$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
 	if (!$conn){
 		die("Connection failed: ".mysqli_conect_error());
