234c2a48d9eedbce9bb8eae234ee0f1c994993a1
Colin Richardson <53791692+Colin-Richardson@users.noreply.github.com>
2021-03-31 11:11:56 -0500

    cleanup

--- shop.php
+++ shop.php
@@ -30,7 +30,7 @@
             <div class="navbar-nav">
                 <a class="nav-item nav-link" href="augmented_arms.php">Augmented Arms</a>
                 <a class="nav-item nav-link" href="cyber_contacts.php">Cyber Contacts</a>
-                <a class="nav-item nav-link" href="exoskeleton.php">ExoSuit</a>
+                <a class="nav-item nav-link" href="exosuit.php">ExoSuit</a>
                 <a class="nav-item nav-link acnav" href="account.php">Account</a>
             </div>
 
