ac2f46f0830fb264e7ed5fa64bf9b663a943f168
Colin Richardson <53791692+Colin-Richardson@users.noreply.github.com>
2021-04-05 09:24:27 -0500

    asdf
    asdf

--- augmented_arms.php
+++ augmented_arms.php
@@ -21,11 +21,11 @@
 <body>
     <!-- Navigation -->
     <div class="armback">
-    <?php
+        <?php
     include "includes/shopnav.php";
     ?>
-    <!-- End Navigation -->
-    <!-- Arms Jumbotron -->
+        <!-- End Navigation -->
+        <!-- Arms Jumbotron -->
         <div class="jumbotron jumbotron-fluid productjumbo">
             <div class="container state">
                 <h1 class="display-3">Augmented Arms</h1>
@@ -35,8 +35,38 @@
     </div>
     <!-- End of Arms Jumbotron -->
     <div class="updown"><br><br><br></div>
+    <!-- Start Quote Jumbotron -->
+    <div class="jumbotron jumbotron-fluid">
+        <h1 class="exodesctit">Our Augmented Arms</h1><br>
+        <p class="exodesc">"This individual piece of our exosuit is designed to bring strength and stability to the
+            arms, focusing on the three main muscles: the biceps, the triceps, and the forearm" - Colin Richardson</p>
+    </div>
+    <!-- End of Quote Jumbotron -->
+    <div class="downup"><br><br><br></div>
     <!-- Start of Row & Column -->
+    <div class="row armbreak">
+        <div class="col-6">
+            <img class="indepth-arm" src="img/in-deptharm.jpg" alt="Arm Breakdown">
+        </div>
+        <div class="col-6 eyebreak"><br><br><br><br>
+            <h1 class="display-5 exobreaktit">In-Depth Specifications</h1><br>
+            <h2 class="state eyebreakh2">The biceps, forearms, and triceps are treated the extreme care so that we do not cause any permanent damage to the arm*</h2>
+            <ul class="armbreakdesc">
+                <br>
+                <li>Enhanced strength when curling objects</li>
+                <br>
+                <li>Added stability to pushing and pulling objects</li>
+                <br>
+                <li>Augmented hands which add greater gripping strength</li>
+            </ul>
+        </div>
+    </div>
     <!-- End of Row & Column -->
+    <!-- Buy -->
+    <div class="jumbotron armbreak"><br><br><br>
+        <h1 class="display-5 exobuytit">Join the movement and contribute to the progression of humanity</h1>
+        <div class="buybutton"><a href="#" class="button1">Buy Now</a></div><br><br><br><br>
+    </div>
     <?php 
         include "includes/footer.php"
     ?>
