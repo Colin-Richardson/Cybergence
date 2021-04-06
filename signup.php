8bf47a50344f5879fd998497e4400ec901f506f7
Colin Richardson <53791692+Colin-Richardson@users.noreply.github.com>
2021-04-06 14:06:37 -0500

    beautify

--- signup.php
+++ signup.php
@@ -22,60 +22,63 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg" id="navbar">
     <a class="navbar-brand" href="shop.php"><img class="logo" src="img/favicon.png" alt="Cybergence"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarcollapse"
-            aria-controls="navbarcollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarcollapse">
-            <div class="navbar-nav">
-                <a class="nav-item nav-link" href="augmented_arms.php">Augmented Arms</a>
-                <a class="nav-item nav-link" href="cyber_contacts.php">Cyber Contacts</a>
-                <a class="nav-item nav-link" href="exosuit.php">ExoSuit</a>
-                <a class="nav-item nav-link sunav" href="signup.php">Sign Up</a>
-                <a class="nav-item nav-link lginnav" href="login.php">Login</a>               
-            </div>
-        </div>
-	</nav>
-  <!-- End Navigation -->
-<div class="signup__container">
-  <div class="container__child signup__thumbnail">
-    <div class="thumbnail__content text-center"><br><br><br><br>
-      <h1 class="display-5 signuph1">Cybergence</h1><br>
-      <h2 class="display-7 signuph2">Join the progression of humanity</h2><br><br><br><br><br>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarcollapse"
+      aria-controls="navbarcollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarcollapse">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link" href="augmented_arms.php">Augmented Arms</a>
+        <a class="nav-item nav-link" href="cyber_contacts.php">Cyber Contacts</a>
+        <a class="nav-item nav-link" href="exosuit.php">ExoSuit</a>
+        <a class="nav-item nav-link sunav" href="signup.php">Sign Up</a>
+        <a class="nav-item nav-link lginnav" href="login.php">Login</a>
+      </div>
     </div>
-    <div class="signup__overlay"></div>
+  </nav>
+  <!-- End Navigation -->
+  <div class="signup__container">
+    <div class="container__child signup__thumbnail">
+      <div class="thumbnail__content text-center"><br><br><br><br>
+        <h1 class="display-5 signuph1">Cybergence</h1><br>
+        <h2 class="display-7 signuph2">Join the progression of humanity</h2><br><br><br><br><br>
+      </div>
+      <div class="signup__overlay"></div>
+    </div>
+    <div class="container__child signup__form">
+      <form action="includes/signup.inc.php" method="post">
+        <div class="form-group">
+          <label class="signuplabel" for="username">Username</label>
+          <input class="form-control" type="text" name="uid" id="username" placeholder="username" required />
+        </div>
+        <div class="form-group">
+          <label class="signuplabel" for="email">Email</label>
+          <input class="form-control" type="text" name="mail" id="email" placeholder="username@cybergence.com"
+            required />
+        </div>
+        <div class="form-group">
+          <label class="signuplabel" for="password">Password</label>
+          <input class="form-control" type="password" name="pwd" id="password" placeholder="********" required />
+        </div>
+        <div class="form-group">
+          <label class="signuplabel" for="passwordRepeat">Repeat Password</label>
+          <input class="form-control" type="password" name="pwd-repeat" id="passwordRepeat" placeholder="********"
+            required />
+        </div>
+        <br>
+        <div class="m-t-lg">
+          <ul class="list-inline">
+            <li>
+              <input class="btn btn--form" type="submit" name="signup-submit" value="Register" />
+            </li>
+            <li>
+              <a class="signup__link signupa" href="login.php">I am already a member</a>
+            </li>
+          </ul>
+        </div>
+      </form>
+    </div>
   </div>
-  <div class="container__child signup__form">
-    <form action="includes/signup.inc.php" method="post">
-      <div class="form-group">
-        <label class="signuplabel" for="username">Username</label>
-        <input class="form-control" type="text" name="uid" id="username" placeholder="username" required />
-      </div>
-      <div class="form-group">
-        <label class="signuplabel" for="email">Email</label>
-        <input class="form-control" type="text" name="mail" id="email" placeholder="username@cybergence.com" required />
-      </div>
-      <div class="form-group">
-        <label class="signuplabel" for="password">Password</label>
-        <input class="form-control" type="password" name="pwd" id="password" placeholder="********" required />
-      </div>
-      <div class="form-group">
-        <label class="signuplabel" for="passwordRepeat">Repeat Password</label>
-        <input class="form-control" type="password" name="pwd-repeat" id="passwordRepeat" placeholder="********" required />
-	  </div>
-	  <br>
-      <div class="m-t-lg">
-        <ul class="list-inline">
-          <li>
-            <input class="btn btn--form" type="submit" name="signup-submit" value="Register" />
-          </li>
-          <li>
-            <a class="signup__link signupa" href="login.php">I am already a member</a>
-          </li>
-        </ul>
-      </div>
-    </form>  
-  </div>
-</div>
 </body>
+
 </html>
