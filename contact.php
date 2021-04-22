05c3412a8c9a0c926c7b6aeb113072b2c24bd458
Matthew Thornton <53791828+Matthew-Thornton@users.noreply.github.com>
2021-04-21 21:46:49 -0500

    cleanup

--- contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-?>
-
-<h1>signup</h1>
-<form action="includes/contact.inc.php" method="post">
-	<textarea type="text" name="msg" placeholder="message"></textarea>
-	<button type="submit" name="contact-submit">Signup</button>
-</form>
--- includes/shopnav.php
+++ includes/shopnav.php
@@ -1,6 +1,6 @@
 
         <nav class="navbar navbar-expand-lg navopacity">
-            <a class="navbar-brand" href="shop.php"><img class="logo" src="img/favicon.png" alt="Cybergence"></a>
+            <a class="navbar-brand" href="index.php"><img class="logo" src="img/favicon.png" alt="Cybergence"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarcollapse"
                 aria-controls="navbarcollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -14,7 +14,11 @@
 		<?php 
 		if (isset($_SESSION['userId']))
 		{
-                   echo '<a class="nav-item nav-link lginnav" href="includes/logout.inc.php">Logout</a>'; 
+			if (isset($_SESSION['admin']))
+			{
+                   		echo '<a class="nav-item nav-link sunav" href="admin.php">admin</a>'; 
+			}
+                   		echo '<a class="nav-item nav-link lginnav" href="includes/logout.inc.php">Logout</a>'; 
 
 		} else
 		{	
